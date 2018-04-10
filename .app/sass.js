7f0b6b3920c37f4984814698261774d8bcb77163
devowhippit <devonhirth@gmail.com>
2018-04-10 12:08:29 -0400

    Add package to write directory paths if they do not exist

--- .app/sass.js
+++ .app/sass.js
@@ -6,25 +6,41 @@ const Sass = require('node-sass');
 const Path = require('path');
 const Fs = require('fs');
 const modules = require('../config/modules');
+const mkdirp = require('mkdirp');
 
 /**
  * Init
  */
 
-modules.forEach(function(module) {
-  let bundle = Path.join(__dirname, '../', module.outDir);
+function write(module) {
+  let outDir = Path.join(__dirname, '../', module.outDir);
   let name = module.outFile;
   Sass.render(module, (err, result) => {
     if (err) {
       console.log(`${err.formatted}`);
     } else {
-      Fs.writeFile(`${bundle}${name}`, result.css, (err) => {
+      Fs.writeFile(`${outDir}${name}`, result.css, (err) => {
         if (err) {
           console.log(err);
         } else {
-          console.log(`SASS written to ${bundle}${name}`);
+          console.log(`SASS written to ${outDir}${name}`);
         }
       });
     }
   });
+}
+
+modules.forEach(function(module) {
+  let outDir = Path.join(__dirname, '../', module.outDir);
+  if (!Fs.existsSync(outDir)) {
+    mkdirp(outDir, (err) => {
+      if (err) {
+        console.error(err);
+      } else {
+        write(module);
+      }
+    });
+  } else {
+    write(module);
+  }
 });
