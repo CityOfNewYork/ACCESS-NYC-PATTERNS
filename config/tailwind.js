2b8c30213d47744723b468f5a018289052ff3c11
devowhippit <devonhirth@gmail.com>
2018-07-23 11:57:43 -0400

    Adding leading utilities

--- config/tailwind.js
+++ config/tailwind.js
@@ -24,6 +24,7 @@ module.exports = {
   fonts: variables.fonts,
   textSizes: variables.fontSizes,
   fontWeights: variables.fontWeights,
+  leading: variables.leading,
   borderWidths: variables.borderWidths,
   margin: variables.margin,
   maxWidth: {
@@ -46,6 +47,7 @@ module.exports = {
     flexbox: ['responsive'],
     fonts: [],
     fontWeights: [],
+    leading: [],
     margin: ['responsive'],
     maxWidth: ['responsive'],
     overflow: ['responsive'],
