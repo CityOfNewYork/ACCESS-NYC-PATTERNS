59314ae0a162f26c09e6b31f07bc5aa2294dae7b
devowhippit <devonhirth@gmail.com>
2020-05-11 10:41:57 -0400

    Testing pa11y checker

---
+++ config/pa11y.js
@@ -0,0 +1,9 @@
+/**
+ * Configuration options for pa11y
+ *
+ * @param  {String}  url  https://github.com/pa11y/pa11y#configuration
+ */
+module.exports = {
+  'standard': 'WCAG2AA',
+  'runners': ['axe', 'htmlcs']
+};
